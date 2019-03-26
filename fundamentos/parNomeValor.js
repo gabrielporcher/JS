24a6d711ac7817073bfe2b47ba24072750a2c2b1
gabrielporcher <gabrielporcher95@gmail.com>
2019-03-26 10:34:50 -0300

    Enviando mudanca

--- fundamentos/parNomeValor.js
+++ fundamentos/parNomeValor.js
@@ -1,2 +1,22 @@
 // par nome/valor 
-const saudacao = 'Opa'
+const saudacao = 'Opa' //Contexto léxico 1
+
+function exec(){
+    const saudacao = 'Ahoee' //Contexto léxico 2
+    return saudacao
+}
+
+//Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua srhsarh',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
