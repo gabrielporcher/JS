a67bc360a83657adff99613a459e43a64143eb8c
gabrielporcher <gabrielporcher95@gmail.com>
2019-03-26 12:48:57 -0300

    + 2

---
+++ fundamentos/notacaoPonto.js
@@ -0,0 +1,19 @@
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola'
+console.log(obj1.nome)
+
+function Obj(nome){
+    this.nome = nome
+    this.exec = function(){
+        console.log('Exec...' + nome)
+    }
+}
+
+const obj2 = new Obj('Sapato')
+console.log(obj2.nome)
+
+const obj3 = new Obj('Monitor')
+console.log(obj3.nome)
+obj3.exec()
